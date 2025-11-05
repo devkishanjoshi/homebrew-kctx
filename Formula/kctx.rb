09f55ee608686fdb16196f7b94ff86cf467a770b
Devkishan Joshi <devkishanjoshi@Devkishans-MacBook-Air.local>
2025-11-05 19:36:28 +0530

    Initial commit

--- Formula/kctx.rb
+++ Formula/kctx.rb
@@ -1,8 +1,8 @@
 class Kctx < Formula
   desc "Kubernetes context switcher CLI written in Python"
   homepage "https://github.com/devkishanjoshi/kctx"
-  url "https://github.com/devkishanjoshi/homebrew-kctx/releases/tag/1.0.1"
-  sha256 "c2f977411fe8db109e8dd3f335607ed8ac26209aef07b7636374664550984cd8"
+  url "https://github.com/devkishanjoshi/homebrew-kctx/archive/refs/tags/1.0.1.tar.gz"
+  sha256 "45c85c5d37f00df6e4f3d94f91ca996a8d89d482e12e7e34992c1326e6094731"
   license "MIT"
 
   depends_on "python@3.11"
